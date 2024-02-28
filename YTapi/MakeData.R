d4f57ea845c4e4298fc0f07c01cb8272f0bc545e
NicoCampa <68902693+NicoCampa@users.noreply.github.com>
2024-02-28 19:01:22 +0100

    init repo

---
+++ YTapi/MakeData.R
@@ -0,0 +1,63 @@
+# Apple Vision Pro
+# 1 - dtp6b76pMak - Using Apple Vision Pro: What It’s Actually Like! 30k comments
+# 2 - UvkgmyfMPks - the thing no one will say about Apple Vision Pro Casey Neistat 13k comments
+# 3 - 8xI10SFgzQ8 - I Spent 24 Hours Wearing Apple’s Vision Pro Headset | WSJ 4k comments
+# 4 - SSC0RkJuBVw - Apple Vision Pro - Is it worth $3500? - 18k comments
+# 5 - SqB0lUcqFbA - Hating Apple is Getting REALLY Hard - WWDC 2023 - 10k comments
+# 6 - hdwaWxY11jQ - Apple Vision Pro review: magic, until it’s not - 5k comments
+# 7 - CaWt6-xe29k - Apple Vision Pro - Unboxing, Review and demos! - 2k comments
+# 8 - M2LWcHIyNWQ - How Apple's Vision Pro Will Change Society Forever - 15k comments
+# 9 - n7hJlyVDEc8 - The Real Reason To Care About The Apple Vision Pro - 2k comments
+# 10 - LmcWMjBpYBU - Be gentle with the Apple Vision Pro - ITS PLASTIC!! - 10k comments
+
+library(tuber)
+yt_oauth(app_id = "879991409679-ogeitgeq8thr77vndveoct6itusktjtv.apps.googleusercontent.com",
+          app_secret = "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", token = "")
+
+MarquesBrownlee <- get_all_comments('dtp6b76pMak')
+CaseyNeistat <- get_all_comments('UvkgmyfMPks')
+WallStreetJournal <- get_all_comments('8xI10SFgzQ8')
+MrWhoseTheBoss <- get_all_comments('SSC0RkJuBVw')
+LinusTechTips <- get_all_comments('SqB0lUcqFbA')
+TheVerge <- get_all_comments('hdwaWxY11jQ')
+iJustine <- get_all_comments('CaWt6-xe29k')
+Moon <- get_all_comments('M2LWcHIyNWQ')
+CleoAbram <- get_all_comments('n7hJlyVDEc8')
+JerryRigEverything <- get_all_comments('LmcWMjBpYBU')
+
+MarquesBrownlee <- cbind('MarquesBrownlee', MarquesBrownlee$textOriginal)
+colnames(MarquesBrownlee) <- c('Channel', 'Comment')
+
+CaseyNeistat <- cbind('CaseyNeistat', CaseyNeistat$textOriginal)
+colnames(CaseyNeistat) <- c('Channel', 'Comment')
+
+WallStreetJournal <- cbind('WallStreetJournal', WallStreetJournal$textOriginal)
+colnames(WallStreetJournal) <- c('Channel', 'Comment')
+
+MrWhoseTheBoss <- cbind('MrWhoseTheBoss', MrWhoseTheBoss$textOriginal)
+colnames(MrWhoseTheBoss) <- c('Channel', 'Comment')
+
+LinusTechTips <- cbind('LinusTechTips', LinusTechTips$textOriginal)
+colnames(LinusTechTips) <- c('Channel', 'Comment')
+
+TheVerge <- cbind('TheVerge', TheVerge$textOriginal)
+colnames(TheVerge) <- c('Channel', 'Comment')
+
+iJustine <- cbind('iJustine', iJustine$textOriginal)
+colnames(iJustine) <- c('Channel', 'Comment')
+
+Moon <- cbind('Moon', Moon$textOriginal)
+colnames(Moon) <- c('Channel', 'Comment')
+
+CleoAbram <- cbind('CleoAbram', CleoAbram$textOriginal)
+colnames(CleoAbram) <- c('Channel', 'Comment')
+
+JerryRigEverything <- cbind('JerryRigEverything', JerryRigEverything$textOriginal)
+colnames(JerryRigEverything) <- c('Channel', 'Comment')
+
+commentsEightFebruary = rbind(CaseyNeistat, CleoAbram, iJustine, JerryRigEverything,
+                              LinusTechTips, MarquesBrownlee, Moon, MrWhoseTheBoss,
+                              TheVerge, WallStreetJournal)
+
+
+write.csv(commentsEightFebruary, 'comments8Feb.csv')
---
+++ YTapi/YoutubeAPI.R
@@ -0,0 +1,323 @@
+library(textclean)
+library(tm)
+library(mgsub)
+library(stringr)
+library(ggplot2)
+library(wordcloud)
+library(tidytext)
+library(dplyr)
+library(tidyr)
+library(ggthemes)
+library(igraph)
+library(ggraph)
+library(widyr)
+library(qdap)
+library(caret)
+library(yardstick)
+library(tuber)
+library(stringr)
+library(stringi)
+
+comments = read.csv("comments8Feb.csv", sep = ',')
+comments = comments[,-1]
+
+# scrap the latest version of emojis
+readLines("https://www.unicode.org/Public/emoji/latest/emoji-test.txt",
+          encoding="UTF-8") %>%
+  stri_subset_regex(pattern = "^[^#]") %>%
+  stri_subset_regex(pattern = ".+") -> emoji
+
+# extract the emoji character
+emoji %>%
+  stri_extract_all_regex(pattern = "# *.{1,2} *") %>%
+  stri_replace_all_fixed(pattern = c("*", "#"),
+                         replacement = "",
+                         vectorize_all=FALSE) %>%
+  stri_trim_both() -> emoji.chars
+
+
+# extract the emoji description
+emoji %>%
+  stri_extract_all_regex(pattern = "#.*$") %>%
+  stri_replace_all_regex(pattern = "^#.*?E\\d+\\.\\d+\\s+",
+                         replacement = " ") -> emoji.descriptions
+
+commentsReplacedEmojis = stri_replace_all_regex(comments$Comment, #replace with yours case
+                              pattern = emoji.chars,
+                              replacement = emoji.descriptions,
+                              vectorize_all=FALSE)
+commentsErasedEmojis = stri_replace_all_regex(comments$Comment, #replace with yours case
+                                              pattern = emoji.chars,
+                                              replacement = " ",
+                                              vectorize_all=FALSE)
+
+comments$ReplacedEmoji = commentsReplacedEmojis
+comments$ErasedEmoji = commentsErasedEmojis
+
+# create corpus
+corpus <- Corpus(VectorSource(comments$ReplacedEmoji))
+
+corpus <- tm_map(corpus, content_transformer(tolower)) #all lowercase
+corpus <- tm_map(corpus, removeNumbers) #remove numbers
+corpus <- tm_map(corpus, removeWords,tm::stopwords('en')) #remove english stopwords 
+corpus <- tm_map(corpus, content_transformer(str_replace_all), '-', ' ') #replace dashes with spaces
+corpus <- tm_map(corpus, content_transformer(str_replace_all), '–', ' ')
+corpus <- tm_map(corpus, removePunctuation) #remove punctuation
+corpus <- tm_map(corpus, stripWhitespace) #remove white spaces
+
+comments$Comment[40]
+content(corpus[[40]])
+
+tdm <- TermDocumentMatrix(corpus) 
+m <- as.matrix(tdm) #matrix
+df <- data.frame(text = sapply(corpus, as.character), stringsAsFactors = FALSE) #data.frame
+v <- sort(rowSums(m),decreasing=TRUE) #term frequencies
+d <- data.frame(word = names(v),freq=v)
+
+# wordcloud
+library(wordcloud)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1000,
+          max.words = 200, random.order = F, rot.per = 0.35,
+          colors = brewer.pal(8, 'Dark2'))
+
+#most frequent words
+barplot(d[1:15,]$freq, las = 2, names.arg = d[1:15,]$word,
+        col ="lightblue", main ="Most frequent words",
+        ylab = "Word frequencies")
+
+#tokenization of cleaned corpus
+wordTokens <- df %>% 
+  unnest_tokens(word, text, drop = FALSE) %>% 
+  select(-text) %>% 
+  filter(!str_detect(word, '\\d')) %>% # delete words with numbers 
+  filter(nchar(word) > 1) # delete words shorter than two letters
+
+load(file = 'sentiments_lexicons.Rdata') #load lexicons
+
+#apply sentiment polarity to words
+wordSentiment <- wordTokens %>% 
+  inner_join(bing, by = c('word' = 'word')) %>% 
+  count(word, sentiment, sort = T) 
+
+#aggregate them
+word_sent_top <- wordSentiment %>% 
+  group_by(sentiment) %>% 
+  top_n(15, n) %>% 
+  ungroup() %>% 
+  mutate(word = reorder(word, n)) 
+
+#plot them, most frequent words by sentiment in BING
+ggplot(word_sent_top, aes(x = word, y = n, fill = sentiment)) +
+  geom_col(show.legend = F) +
+  facet_grid(~sentiment, scales = 'free_x') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+data_sent <- wordTokens %>% 
+  inner_join(bing) %>% 
+  count(sentiment, sort = T) %>% 
+  spread(sentiment, n) %>% 
+  mutate(diff = positive - negative) 
+scoreSentiment <- data.frame(t(data_sent))
+colnames(scoreSentiment) <- 'value'
+t(scoreSentiment)
+
+nrc_polarity <- nrc %>% 
+  filter(sentiment %in% c('positive', 'negative'))
+
+word_sent2 <- wordTokens %>% 
+  inner_join(nrc_polarity, by = c('word' = 'word')) %>% 
+  count(word, sentiment, sort = T) 
+
+word_sent_top <- word_sent2 %>% 
+  group_by(sentiment) %>% 
+  top_n(15, n) %>% 
+  ungroup() %>% 
+  mutate(word = reorder(word, n)) 
+###most frequent words by sentiment in NRC
+ggplot(word_sent_top, aes(x = word, y = n, fill = sentiment)) +
+  geom_col(show.legend = F) +
+  facet_grid(~sentiment, scales = 'free_x') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+data_sent3 <- wordTokens %>% 
+  inner_join(nrc) %>% 
+  count(sentiment, sort = T) %>% 
+  spread(sentiment, n)
+scoreSentiment3 <- data.frame(t(data_sent3))
+colnames(scoreSentiment3) <- c('value')
+pl <- colnames(t(scoreSentiment3))
+plot <- as.data.frame(cbind(pl, as.numeric(t(scoreSentiment3))))
+colnames(plot) <- c('emotion', 'value')
+plot$value <- as.numeric(plot$value)
+
+ggplot(data=plot, aes(x=emotion, y=value)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+                                    axis.title.x =element_blank()) + geom_bar(stat="identity", fill=c('white','white',
+                                                                                                      'white','white',
+                                                                                                      'white','#ff7473',
+                                                                                                      '#28bfc9','white',
+                                                                                                      'white','white'), colour="black") + theme(legend.position = "none")
+associations<-findAssocs(tdm, 'problem', 0.25)
+associations<-as.data.frame(associations)
+associations$terms<-row.names(associations)
+associations$terms<-factor(associations$terms,
+                           levels=associations$terms)
+
+ggplot(associations, aes(y=terms)) +
+  geom_point(aes(x=problem), data=associations,
+             size=5)+
+  theme_gdocs()+ geom_text(aes(x=problem,
+                               label=problem),
+                           colour="darkred",hjust=-.25,size=8)+
+  theme(text=element_text(size=20),
+        axis.title.y=element_blank())
+
+associations<-findAssocs(tdm, 'hate', 0.16)
+associations<-as.data.frame(associations)
+associations$terms<-row.names(associations)
+associations$terms<-factor(associations$terms,
+                           levels=associations$terms)
+
+ggplot(associations, aes(y=terms)) +
+  geom_point(aes(x=hate), data=associations,
+             size=5)+
+  theme_gdocs()+ geom_text(aes(x=hate,
+                               label=hate),
+                           colour="darkred",hjust=-.25,size=8)+
+  theme(text=element_text(size=20),
+        axis.title.y=element_blank())
+
+# BI-GRAMS
+corpusBi <- Corpus(VectorSource(comments$ErasedEmoji))
+
+corpusBi <- tm_map(corpusBi, content_transformer(tolower)) #all lowercase
+corpusBi <- tm_map(corpusBi, removeNumbers) #remove numbers
+corpusBi <- tm_map(corpusBi, removeWords, c(tm::stopwords('en'),'app')) #remove english stopwords 
+corpusBi <- tm_map(corpusBi, content_transformer(str_replace_all), '-', ' ') #replace dashes with spaces
+corpusBi <- tm_map(corpusBi, content_transformer(str_replace_all), '–', ' ')
+corpusBi <- tm_map(corpusBi, removePunctuation) #remove punctuation
+corpusBi <- tm_map(corpusBi, stripWhitespace) #remove white spaces
+
+dfBi <- data.frame(text = sapply(corpusBi, as.character), stringsAsFactors = FALSE)
+
+bigrams <- dfBi %>% 
+  unnest_tokens(bigram, text, token = 'ngrams', n = 2)  %>% 
+  separate(bigram, c('c1', 'c2'), sep = " " )
+
+#most frequent bigrams
+bigram_counts <- bigrams %>% 
+  count(c1, c2, sort = TRUE) #the emoji play a big role in that...
+#i'll see how to adress it..
+
+bigram_counts <- bigram_counts %>%
+  drop_na(c1) %>%
+  drop_na(c2)
+head(bigram_counts, 10)
+
+bigram_graph <- bigram_counts %>% 
+  filter(n > 170) %>% # chooses bigrams that occur more than 35 times
+  graph_from_data_frame() 
+
+
+set.seed(1) 
+#graph of bigrams
+a <- grid::arrow(type = 'closed', length = unit(.1, 'inches')) # we want to have arrow on the plot
+
+set.seed(1)
+ggraph(bigram_graph, layout = "fr") + # to apply a layout to a graph
+  geom_edge_link(aes(edge_alpha = n), # to make links more or less transparent, based on how frequent they appear
+                 show.legend = FALSE,
+                 arrow = a, # we will have arrows on the plot
+                 end_cap = circle(.07, 'inches')) + # arrows will end before the touching the node
+  geom_node_point(color = "red", size = 1) + 
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1) + # to add names to the plot
+  theme_void()
+
+#see which words are preceded by a negation
+negation_words <- c('headset', 'price', 'immersion', 'vr', 'virtual', 'buy') 
+negated_words <- bigrams %>% 
+  filter(c1 %in% negation_words) %>% 
+  inner_join(afinn, by = c('c2' = 'word')) %>% 
+  count(c1, c2, value, sort = TRUE) %>% 
+  ungroup
+
+
+top_neg_word <- negated_words %>% 
+  mutate(contribution = n * value) %>% 
+  arrange(desc(abs(contribution))) %>% 
+  group_by(c1) %>% 
+  top_n(10, abs(contribution)) %>% 
+  ungroup() %>% 
+  mutate(c2 = reorder(c2, contribution)) 
+#plotting the frequences
+ggplot(top_neg_word, aes(c2, n * value, fill = n * value > 0)) +
+  geom_col(show.legend = FALSE) +
+  xlab("Words preceded by negation") +
+  ylab("Sentiment score * number of occurrences") +
+  facet_wrap(~c1, ncol = 2, scales = 'free') + 
+  coord_flip()
+
+# ### **2.7 Correlation** 
+# library(widyr)
+#data without emojis
+
+corpus2 <- Corpus(VectorSource(comments$ErasedEmoji))
+# 
+corpus2 <- tm_map(corpus2, content_transformer(tolower)) #all lowercase
+corpus2 <- tm_map(corpus2, removeNumbers) #remove numbers
+corpus2 <- tm_map(corpus2, removeWords, c(tm::stopwords('en'))) #remove english stopwords 
+corpus2 <- tm_map(corpus2, content_transformer(str_replace_all), '-', ' ') #replace dashes with spaces
+corpus2 <- tm_map(corpus2, content_transformer(str_replace_all), '–', ' ')
+corpus2 <- tm_map(corpus2, removePunctuation) #remove punctuation
+corpus2 <- tm_map(corpus2, stripWhitespace) #remove white spaces
+# 
+df2 <- data.frame(text = sapply(corpus2, as.character), stringsAsFactors = FALSE)
+# # 
+
+df2$review <- rep(c(1:6938),each = 14)
+t = df2 %>% unnest_tokens(word, text, drop = FALSE) %>% 
+select(-text) %>%
+group_by(word) %>%
+filter(n() >= 20) %>%
+pairwise_cor(word, review, sort = TRUE) 
+# 
+# 
+# 
+# 
+# 
+# # We can pick some interesting words and find other most associated with them:
+word_cors_top6 <- t %>%
+  filter(item1 %in% c("expensive", "problem", "hate", "battery")) %>% # we choose those words
+group_by(item1) %>% 
+top_n(10) %>% # we choose top 10 (frequent) words 
+ungroup() %>%
+mutate(item2 = reorder(item2, correlation)) # new variable, that arranges second word acording to correlation
+# 
+# # graph With colors:
+ggplot(word_cors_top6, aes(item2, correlation, fill = correlation)) + 
+geom_col() + 
+facet_wrap(~ item1, scales = "free") + # to organize plots
+coord_flip() # flips orientation
+# # graph With colors:
+ggplot(word_cors_top6, aes(item2, correlation, fill = correlation)) + 
+geom_col() + 
+facet_wrap(~ item1, scales = "free") + # to organize plots
+coord_flip() # flips orientation
+# 
+# 
+# # We can now visualize the correlations and clusters of words:
+# set.seed(2016)
+# 
+# # Let's use correlation matrix for network analysis:
+t %>% filter(correlation > .50) %>% # filters data 
+ graph_from_data_frame() %>%
+ ggraph(layout = "fr") +
+ geom_edge_link(show.legend = FALSE) +
+ geom_node_point(color = "#277BC0", size = 4) +
+ geom_node_text(aes(label = name), repel = TRUE) +
+ theme_void()
+# 
+# # You can see that the output is rather symmetrical and there are no arrows - because
+# # the relationship isn't directional. 
+# # common (like in the bigram analysis earlier)
+
